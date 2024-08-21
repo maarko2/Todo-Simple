9978098007c730a6a52eca63f38d9d53b28dfee4
maarko2 <maarko.burgos@gmail.com>
2024-08-21 14:02:12 -0400

    Proyecto Base

---
+++ Simplicity-Phone/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Simplicity-Phone',
+  webDir: 'www'
+};
+
+export default config;
