86d2c0f478e4241bdd7044468e3ecf5f95e55730
maarko <maarko.burgos@gmail.com>
2024-10-17 00:26:46 -0300

    Ajustes de Codigo

--- Simplicity-Phone/src/app/recordatorios/recordatorios.component.ts
+++ Simplicity-Phone/src/app/recordatorios/recordatorios.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UtilsService } from 'src/app/services/utils.service';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recordatorios',
@@ -7,7 +8,47 @@ import { UtilsService } from 'src/app/services/utils.service';
   styleUrls: ['./recordatorios.component.scss'],
 })
 export class RecordatoriosComponent {
-  mostrarFormulario = false;
   userName: string | null = null;
+
+  constructor(private utilsSvc: UtilsService, private actionSheetCtrl: ActionSheetController) {
+    this.getUserName();
+  }
+
+  getUserName() {
+    const user = this.utilsSvc.getFromLocalStorage('user');
+    if (user) {
+      this.userName = user.name;
+    }
+  }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Opciones',
+      buttons: [
+        {
+          text: 'Crear Recordatorio',
+          data: {
+            action: '',
+          },
+        },
+        {
+          text: 'Eliminar Recordatorio',
+          role: 'destructive',
+          data: {
+            action: 'delete',
+          },
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
+
+    await actionSheet.present();
+  }
 
 }
